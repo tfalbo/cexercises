f16c62db8c35d286b6d123eb3fafe238f5fb561b
Thaissa Falbo <thaissa.falbo@gmail.com>
2014-03-31 00:04:11 -0300

    Program 2 added

---
+++ prog2.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+/* Print average between 12,25 and 53 */
+
+
+int main()
+{
+	
+	int average;
+	average = (12+25+53)/3;
+	printf("%d\n",average);
+	/* or alternative... */
+	printf("%d\n", (12+25+53)/3);
+	return 0;
+
+}
